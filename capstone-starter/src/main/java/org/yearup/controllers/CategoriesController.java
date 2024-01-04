dcca1d4fa926ce134dc93734bba330ed69eb85ac
Hally Amor <Hamor@apdpev.yearup.org>
2024-01-04 14:35:54 -0500

    Structure development for version 2

---
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,90 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.CategoryDao;
+import org.yearup.data.ProductDao;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@RequestMapping("categories")
+@CrossOrigin
+public class CategoriesController {
+    private CategoryDao categoryDao;
+    private ProductDao productDao;
+
+
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
+    @GetMapping
+    @PreAuthorize("permitAll()")
+    public List<Category> getAll() {
+        return categoryDao.getAllCategories();
+    }
+
+
+    @GetMapping("{id}")
+    @PreAuthorize("permitAll()")
+    public Category getById(@PathVariable int id, HttpServletResponse response) {
+        Category cat = categoryDao.getById(id);
+
+        if (cat == null) {
+            response.setStatus(HttpStatus.NOT_FOUND.value());
+        }
+
+        return cat;
+    }
+
+
+    @GetMapping("{categoryId}/products")
+    @PreAuthorize("permitAll()")
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+        return productDao.listByCategoryId((categoryId));
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Category addCategory(@RequestBody Category category) {
+        try {
+            return categoryDao.create(category);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
+        try {
+            categoryDao.update(id, category);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public void deleteCategory(@PathVariable int id, HttpServletResponse response) {
+
+        if (categoryDao.getById(id) == null) {
+            response.setStatus(HttpStatus.NOT_FOUND.value());
+        } else {
+            categoryDao.delete(id);
+            response.setStatus(HttpStatus.NO_CONTENT.value());
+        }
+    }
+}
